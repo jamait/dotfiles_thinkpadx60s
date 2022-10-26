73e05ec78e0b3d0fe15de424c3e13b57f0d01f7b
nitram <nitram@debian-BULLSEYE-live-builder-AMD64>
2022-10-26 17:07:10 +0200

    Update .local/share/chezmoi/.chezmoiscripts/run_once_install_packages.sh

--- dot_local/private_share/chezmoi/dot_chezmoiscripts/literal_run_once_install_packages.sh
+++ dot_local/private_share/chezmoi/dot_chezmoiscripts/literal_run_once_install_packages.sh
@@ -2,7 +2,6 @@
 sudo apt install -y ripgrep picom
 sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 # Install alacritty ToDo: Remove git?
-#git clone https://github.com/alacritty/alacritty.git
-#cd alacritty
-#git checkout <branch - release name>
-#cargo build --release
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+cargo build --release

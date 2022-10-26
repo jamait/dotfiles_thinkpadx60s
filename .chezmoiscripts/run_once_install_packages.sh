b1c47a4f50751af28190039b8496bb4ed1615b44
nitram <nitram@debian-BULLSEYE-live-builder-AMD64>
2022-10-26 22:37:06 +0200

    install

--- .chezmoiscripts/run_once_install_packages.sh
+++ .chezmoiscripts/run_once_install_packages.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 sudo apt install -y ripgrep picom
-sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-# Install alacritty ToDo: Remove git?
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
-git checkout <branch - release name>
-cargo build --release
+sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev\
+libxcb-xfixes0-dev libxkbcommon-dev python3

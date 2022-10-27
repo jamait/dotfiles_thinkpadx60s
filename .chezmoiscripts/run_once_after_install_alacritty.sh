da2da915d5f71d40b336e5ae83d435a9b9941ec8
nitram <nitram@debian-BULLSEYE-live-builder-AMD64>
2022-10-27 10:33:06 +0200

    alacritty script change

--- .chezmoiscripts/run_once_after_install_alacritty.sh
+++ .chezmoiscripts/run_once_after_install_alacritty.sh
@@ -1,4 +1,4 @@
 sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
+# sudo desktop-file-install extra/linux/Alacritty.desktop
+# sudo update-desktop-database

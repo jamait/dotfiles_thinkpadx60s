59af1ed095425d8337b5ed9a1db3061442a10269
nitram <nitram@debian-BULLSEYE-live-builder-AMD64>
2022-10-27 11:32:43 +0200

    type

--- .chezmoiscripts/run_once_install_packages.sh
+++ .chezmoiscripts/run_once_install_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt install -y xorg ranger chromium neofetch i3 i3locks\
+sudo apt install -y xorg ranger chromium neofetch i3 i3locks\ 
 ripgrep picom cmake pkg-config python3 pulseaudio

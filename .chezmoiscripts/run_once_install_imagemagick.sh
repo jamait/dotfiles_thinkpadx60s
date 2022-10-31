4e078389595a1474635d9b736e4f5bde1fba5dc8
nitram <nitram@debian-BULLSEYE-live-builder-AMD64>
2022-11-01 00:36:32 +0100

    image

---
+++ .chezmoiscripts/run_once_install_imagemagick.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget https://imagemagick.org/archive/ImageMagick.tar.gz
+tar xvzf ImageMagick.tar.gz
+cd ImageMagick-7.1.0-45/
+./configure 
+make
+sudo make install 
+sudo ldconfig /usr/local/lib
+magick -version
